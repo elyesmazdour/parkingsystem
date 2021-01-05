e97c2f2e1b96c2a707c7e7df16ee33e6e6c1be16
Elyes Mahou <elyes.mahou@yahoo.fr>
2021-01-05 23:38:10 +0100

    add test 30 mintues free

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -130,4 +130,18 @@ public class FareCalculatorServiceTest {
 		assertEquals(24 * Fare.CAR_RATE_PER_HOUR, ticket.getPrice());
 	}
 
+	@Test
+	public void calculateFareCarWithLessThanThirtyMinutesParkingTime() {
+		final Date inTime = new Date();
+		inTime.setTime(System.currentTimeMillis() - 30 * 60 * 1000);// 30 minutes parking time should give free parking
+
+		final Date outTime = new Date();
+		final ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+
+		ticket.setInTime(inTime);
+		ticket.setOutTime(outTime);
+		ticket.setParkingSpot(parkingSpot);
+		fareCalculatorService.calculateFare(ticket);
+		assertEquals(0 * Fare.CAR_RATE_PER_HOUR, ticket.getPrice());
+	}
 }
